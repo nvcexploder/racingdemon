da1c01ad565849de06f41a02d700506e4c2f158b
curious-attempt-bunny <curious.attempt.bunny@gmail.com>
2011-09-22 23:01:37 +0000

    fixed static keyword reserved annoyance

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.configure(function() {
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(app.router);    
-    app.use(express.static(__dirname + "/public"));
+    app.use(express['static'](__dirname + "/public"));
 });
 
 var port = process.env.C9_PORT || process.env.PORT;
