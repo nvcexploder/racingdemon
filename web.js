1b1eb352186e2ff08b21c2fc2b7af4f8f9b74747
Chris Jones <chris.f.jones@gmail.com>
2011-09-22 22:38:56 +0000

    initial express server and shell for running in cloud9 and/or heroku

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);    
+    app.use(express.static(__dirname + "/public"));
+});
+
+var port = process.env.C9_PORT || process.env.PORT;
+app.listen(port, '0.0.0.0', function() {
+    console.log("Listening on " + port);
+});
