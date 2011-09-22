ad39554d30af5086309b6a3a2ef75cc805dc0d38
Chris Jones <chris.f.jones@gmail.com>
2011-09-22 23:04:17 +0000

    socket.io shell support

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var app = express.createServer(express.logger());
+var app     = express.createServer(express.logger());
+var io      = require('socket.io').listen(app);
 
 app.configure(function() {
     app.use(express.methodOverride());
@@ -8,7 +9,15 @@ app.configure(function() {
     app.use(express.static(__dirname + "/public"));
 });
 
+io.sockets.on('connection', function (socket) {
+  socket.emit('client', { message: "from server" });
+  socket.on('server', function (data) {
+    console.log(data.message);
+  });
+});
+
 var port = process.env.C9_PORT || process.env.PORT;
 app.listen(port, '0.0.0.0', function() {
     console.log("Listening on " + port);
 });
+
